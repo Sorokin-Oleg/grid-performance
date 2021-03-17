e43c203358897796ee908010b074279985ea0e3f
Sorokin-Oleg <sorokin.world@gmail.com>
2021-03-17 11:01:20 +0300

    [dev] Added const

--- util/consts.js
+++ util/consts.js
@@ -1,2 +1,3 @@
-export const ROWS_COUNT = 10000;
+export const ROWS_COUNT = 100000;
 export const MEASURING_COUNT = 5;
+export const DEFINE_COLUMNS = { firstName: 10, surname: 10, city: 10, age: 20, color: 20, score: 10, done: 10, rating: 10 };
--- webix/unlocked.js
+++ webix/unlocked.js
@@ -1,12 +1,11 @@
 import { RenderTimer, FPS, Scroller } from '../util/util.js';
+import { DEFINE_COLUMNS } from '../util/consts.js';
 import ColumnsGenerator from './util.js';
 import DataGenerator from '../util/dataGenerator.js';
 
 async function init() {
-    const defineColumns = { firstName: 10, surname: 10, city: 10, age: 20, color: 20, score: 10, done: 10, rating: 10 };
-    // const defineColumns = { firstName: 50, surname: 50, city: 50, age: 100, color: 100, score: 50, done: 50, rating: 50 };
-    const columns = ColumnsGenerator.getDefinedColumns(defineColumns);
-    const json = DataGenerator.getDefinedData(defineColumns);
+    const columns = ColumnsGenerator.getDefinedColumns(DEFINE_COLUMNS);
+    const json = DataGenerator.getDefinedData(DEFINE_COLUMNS);
 
     RenderTimer.start({
         callback() {
